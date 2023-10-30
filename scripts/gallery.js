7b220e251390676ab408618b68c2eb68746a82ec
nrb73 <85528596+nrb73@users.noreply.github.com>
2023-10-30 13:51:38 +0000

    done with class

---
+++ scripts/gallery.js
@@ -0,0 +1,17 @@
+
+
+$("#gallery-thumb2").click (function() {
+
+    $("#gallery-figure1").addClass("hidden");
+    $("#gallery-figure2").removeClass("hidden");
+    $("#gallery-figure3").addClass("hidden");
+
+})
+
+$("#gallery-thumb2").click (function() {
+
+    $("#gallery-figure1").addClass("hidden");
+    $("#gallery-figure2").removeClass("hidden");
+    $("#gallery-figure3").addClass("hidden");
+
+})
